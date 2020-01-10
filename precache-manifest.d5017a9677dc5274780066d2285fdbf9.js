e5b7340f852ec539937583e978bf29018a04e7f2
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2020-01-11 01:51:28 +0300

    Updates

---
+++ precache-manifest.d5017a9677dc5274780066d2285fdbf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a48bd555cf07803c4b2624c3e64d83",
+    "url": "//react-mobx-typescript-crud/index.html"
+  },
+  {
+    "revision": "8b1aae1f1402ac22c963",
+    "url": "//react-mobx-typescript-crud/static/css/main.f2d36aa7.chunk.css"
+  },
+  {
+    "revision": "946739828d9e4197d0ba",
+    "url": "//react-mobx-typescript-crud/static/js/2.0c57e583.chunk.js"
+  },
+  {
+    "revision": "ea8761bfdc77ba82ac0139b0e0ab83b5",
+    "url": "//react-mobx-typescript-crud/static/js/2.0c57e583.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b1aae1f1402ac22c963",
+    "url": "//react-mobx-typescript-crud/static/js/main.30b3b6f5.chunk.js"
+  },
+  {
+    "revision": "40142b1a877b2778dfaf",
+    "url": "//react-mobx-typescript-crud/static/js/runtime-main.b5989836.js"
+  }
+]);
